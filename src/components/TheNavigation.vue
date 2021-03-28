ebf54d3df8b30090608896d1e8bc8b25ae62da05
jason <jason@thinkypad.attlocal.net>
2021-03-28 15:44:03 -0500

    created navigation component

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer />
+      <v-toolbar-title>WeatherVue</v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TheNavigation',
+  data: () => ({ drawer: false }),
+};
+</script>

0ac3a75b8c203bf58b95969315719ed1993a6bf4
jason <jason@thinkypad.attlocal.net>
2021-04-03 17:30:35 -0500

    added current, min, and max temps

--- src/views/CurrentForecast.vue
+++ src/views/CurrentForecast.vue
@@ -1,26 +1,52 @@
 <template>
-  <div>
-    <v-card class="mx-auto" color="#26c6da" dark max-width="400">
-      <v-card-title>
-        <span class="title">{{ currentForecast.name }}</span>
-      </v-card-title>
-      <v-card-text> Temp: {{ currentForecast.main.temp }} </v-card-text>
-
-      <v-list-item two-line>
+  <div v-if="currentForecast.isLoaded" class="cf-container">
+    <v-card
+      class="mx-auto"
+      max-width="600"
+      :loading="!currentForecast.isLoaded"
+    >
+      <v-list-item three-line>
         <v-list-item-content>
           <v-list-item-title class="headline">
-            {{ currentForecast.name }}
+            {{ currentForecast.data.name }}
           </v-list-item-title>
-          <v-list-item-subtitle
-            >{{ currentDate }},
-            {{ currentForecast.weather[0].description }}</v-list-item-subtitle
-          >
+          <v-list-item-subtitle>{{ currentDate }} </v-list-item-subtitle>
+          <v-list-item-subtitle>
+            {{ currentForecast.data.weather[0].description }}
+          </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
+      <v-container fill-height>
+        <v-row align="center" justify="center">
+          <v-col cols="6" class="d-flex justify-center">
+            <p>{{ currentForecast.data.main.temp }}&deg;F</p>
+          </v-col>
+          <v-col cols="6" class="d-flex justify-center">
+            <v-icon large color="blue-grey darken-3">{{ weatherIcon }}</v-icon>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-container fill-height>
+        <v-row align="center" justify="center">
+          <v-col cols="6" class="d-flex justify-center">
+            <div>
+              <h1>High</h1>
+              <p>{{ currentForecast.data.main.temp_max }}&deg;F</p>
+            </div>
+          </v-col>
+          <v-col cols="6" class="d-flex justify-center">
+            <div>
+              <h1>Low</h1>
+              <p>{{ currentForecast.data.main.temp_min }}&deg;F</p>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-card>
   </div>
 </template>
 <script>
+import { getCurrentDate, getWeatherIcon } from '../util/utils';
 export default {
   name: 'CurrentForecast',
   data: () => ({
@@ -31,31 +57,10 @@ export default {
       return this.$store.state.currentForecast;
     },
     currentDate() {
-      const months = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-      ];
-
-      const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-      const timeElapsed = Date.now();
-      const now = new Date(timeElapsed);
-
-      const monthIndex = now.getMonth();
-      const year = now.getFullYear();
-      const date = now.getDate();
-      const monthName = months[monthIndex];
-      const dayName = days[now.getDay()];
-      return `${dayName}, ${date} ${monthName} ${year}`;
+      return getCurrentDate();
+    },
+    weatherIcon() {
+      return getWeatherIcon(this.currentForecast.data.weather[0].icon);
     },
   },
   created() {
@@ -63,4 +68,8 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.cf-container {
+  padding: 3em;
+}
+</style>

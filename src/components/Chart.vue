1bb8d55d16052dcbc8ee7b775c401e9bbfea04e3
jason <jason@thinkypad.attlocal.net>
2021-04-18 19:24:50 -0500

    added highcharts lib. intial five day chart logic

---
+++ src/components/Chart.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="options">
+    <highcharts class="hc" :options="options" ref="chart"></highcharts>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['options'],
+  data() {
+    return {
+      chartOptions: {
+        title: {
+          text: 'testing',
+        },
+        xAxis: {
+          categories: ['uno', 'dos', 'tres'],
+        },
+        series: [
+          {
+            data: [1, 2, 3],
+          },
+        ],
+      },
+    };
+  },
+  methods: {},
+  mounted() {
+    console.log(this.options);
+  },
+};
+</script>

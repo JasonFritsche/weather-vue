38c191e21255125b92fd4161c03878463dc9aa66
jason <jason@thinkypad.attlocal.net>
2021-04-19 20:51:48 -0500

    moved search to toolbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,19 +3,16 @@
     <TheNavigation />
     <TheToolbar></TheToolbar>
     <v-container fluid>
-      <TheSearch></TheSearch>
       <router-view></router-view>
     </v-container>
   </v-main>
 </template>
 <script>
-import TheSearch from '../components/TheSearch';
 import TheToolbar from '../components/TheToolbar';
 import TheNavigation from '../components/TheNavigation';
 export default {
   name: 'Home',
   components: {
-    TheSearch,
     TheToolbar,
     TheNavigation,
   },
